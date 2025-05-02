e86eee187da36a35e1ec1f22ac634b9a3f3ba3ba
Syphurus <146368398+Syphurus@users.noreply.github.com>
2025-05-02 16:06:16 +0530

    try11

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client"; // NOT from generated path
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
